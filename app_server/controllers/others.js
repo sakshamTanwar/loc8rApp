9d46900c5ec205704ca72943a411fe07889136c1
Saksham Tanwar <saksham.tanwar608@gmail.com>
2018-10-17 17:11:24 +0530

    Shift data from View to Controller

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,10 @@
 /* About Page */
 module.exports.about = (req, res) => {
-    res.render('generic-text', { title: 'About' });
+
+    var context_dic = {
+        title: 'About',
+        text: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem acnisi dignissim accumsan.'
+    }
+
+    res.render('generic-text', context_dic);
 };
