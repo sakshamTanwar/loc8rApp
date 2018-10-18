71d4edefbb224af8b2abed36b2033187b587c63b
Saksham Tanwar <saksham.tanwar608@gmail.com>
2018-10-18 15:13:06 +0530

    Add Mongoose to use Models with MongoDB

---
+++ app_server/models/db.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+require('./locations');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+
+if(process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGODB_URI;
+};
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error : ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+var gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+process.once('SIGURS2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGURS2');
+    });
+});
+
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    })
+})
+
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
